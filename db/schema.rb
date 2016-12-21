5984a47e664476a7fe19d00cf4c2d9f20df812d4
rayane <rayanesantos-131@hotmail.com>
2016-12-20 20:52:55 -0500

    ajuste em users table 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221012719) do
+ActiveRecord::Schema.define(version: 20161221015210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20161221012719) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "password"
-    t.string   "phone"
     t.date     "birthday"
     t.string   "phone_number"
   end
