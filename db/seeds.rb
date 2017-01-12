934ce50d50ae2eabd8011244bb06dcfa0fc7e0a1
lucas <lucas64_64@hotmail.com>
2017-01-11 23:04:50 -0300

    Seeds adicionados

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,31 @@
 Admin.create(name: 'Admin GTI', email: 'admin@admin.com', password: '123456')
 
+Category.create(name: "Matemática")
+Category.create(name: "Português")
+Category.create(name: "Biologia")
+Category.create(name: "História")
+Category.create(name: "Física")
 # Criação de users apenas para teste de paginação.
+Subcategory.create(name: "Geometria", category: Category.find_by(name: "Matemática"))
+Subcategory.create(name: "Cinemática", category: Category.find_by(name: "Física"))
+Subcategory.create(name: "Trigonometria", category: Category.find_by(name: "Matemática"))
+Subcategory.create(name: "Dinâmica", category: Category.find_by(name: "Física"))
+
+# Content.create(title: "Lei dos Senos", subcategory: Subcategory.find_by(name: "Geometria"), file: "x.pdf")
+# Content.create(title: "Lei dos Cossenos", subcategory: Subcategory.find_by(name: "Geometria"),file: "x.pdf")
+# Content.create(title: "Volumes", subcategory: Subcategory.find_by(name: "Geometria"),file: "x.pdf")
+
+Teacher.create(name: "Osvaldo", category: Category.find_by(name: "Matemática"))
+Teacher.create(name: "Célio Normando", category: Category.find_by(name: "Matemática"))
+Teacher.create(name: "Tarcísio Maciel", category: Category.find_by(name: "Matemática"))
+Teacher.create(name: "Rudini", category: Category.find_by(name: "Matemática"))
+
+Lesson.create(subcategory: Subcategory.find_by(name: "Geometria"),teacher: Teacher.find_by(name: "Rudini"),date_t: "2017-01-11",lesson_hour_start: '04:00-PM',lesson_hour_end:'05:00-PM',limit: 10)
+Lesson.create(subcategory: Subcategory.find_by(name: "Trigonometria"),teacher: Teacher.find_by(name: "Célio Normando"),date_t: "2017-01-11",lesson_hour_start: '04:00-PM',lesson_hour_end:'05:00-PM',limit: 10)
+Lesson.create(subcategory: Subcategory.find_by(name: "Geometria"),teacher: Teacher.find_by(name: "Osvaldo"),date_t: "2017-01-11",lesson_hour_start: '06:00-PM',lesson_hour_end:'08:00-PM',limit: 10)
+Lesson.create(subcategory: Subcategory.find_by(name: "Geometria"),teacher: Teacher.find_by(name: "Rudini"),date_t: "2017-01-11",lesson_hour_start: '06:00-PM',lesson_hour_end:'10:00-PM',limit: 10)
+
+
 
 User.create(name: "Lucas", email: "lucas@gmail.com",password: '123465',birthday: "18-03-1996")
 User.create(name: "Rayane", email: "rayane@gmail.com",password: '123465',birthday: "18-03-1996")
