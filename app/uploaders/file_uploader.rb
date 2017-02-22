3d34b8891679eb7ae197fed71d9828ad444abfd8
rayane <rayanesantos-131@hotmail.com>
2017-02-22 09:08:44 -0300

    configamazon

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -5,7 +5,14 @@ class FileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  end
+
+ # Use AWS storage if in production
+ if Rails.env.production?
+   storage :fog
+ end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
