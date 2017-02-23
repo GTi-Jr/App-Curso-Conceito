a4a2ab87b438dd0892983f3938c4f7a53a34e86b
Patrick Magal <pixelzip0@gmail.com>
2017-02-23 17:49:06 -0300

    indentação e deleted css,js default

--- app/controllers/api/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Api::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     if @user.persisted? && !@user.blocked
       sign_in @user, :event => :authentication #this will throw if @user is not activated
       #set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      render :status => 200, :json => {success: true, name: @user.name, image: @user.image, birthday: @user.birthday }	
+      render :status => 200, :json => {success: true, name: @user.name, image: @user.image, birthday: @user.birthday }
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       #redirect_to new_user_registration_url
@@ -15,7 +15,7 @@ class Api::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
   end
 
   def check_error_blocked
-    @user = User.from_omniauth(request.env["omniauth.auth"]) 
-    @user.errors.add(:blocked, :invalid, message: "true") 
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.errors.add(:blocked, :invalid, message: "true")
   end
 end
