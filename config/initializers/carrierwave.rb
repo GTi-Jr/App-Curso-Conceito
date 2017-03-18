d3c1587cfe4884f85225866fdb85c1b78e292c96
Patrick Magal <pixelzip0@gmail.com>
2017-03-18 08:49:32 -0300

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["S3_KEY_ID"],            # required
-    aws_secret_access_key:  ENV["S3_SECRET_KEY"],            # required
+    aws_access_key_id:     Rails.application.secrets.s3_access_key_id,            # required
+    aws_secret_access_key: Rails.application.secrets.s3_secret_access_key,            # required
     region:                'sa-east-1',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'cursoconceito'                          # required
