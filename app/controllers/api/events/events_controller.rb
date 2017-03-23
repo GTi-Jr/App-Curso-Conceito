4827a625be5b98d3bf94a97643b31576f70d2bcc
Patrick Magal <pixelzip0@gmail.com>
2017-03-22 23:11:13 -0300

    events

--- app/controllers/api/events/events_controller.rb
+++ app/controllers/api/events/events_controller.rb
@@ -1,21 +1,19 @@
 class Api::Events::EventsController < ApplicationApiController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+  #verificar se esta logado
   before_action :user_api
-  before_action :set_user_create, only: :create
   before_action :set_event_edit, only: :edit
   before_action :filter_week, except: :index
-  before_action :check_conflicts_time, except: :index
+  before_action :check_conflicts_time, only: [:create, :edit]
 
   def index
-    pag            = params[:page]           ? params[:page]           : 1
-    limit          = params[:limit]          ? params[:limit]          : 30
+    pag = params[:page] || 1
+    limit = params[:limit]  || 30
     events =  ::Event.paginate(:page => pag, :per_page => limit).where(user_id: current_user.id)
     render :status => 200, :json => {success: true, limit: limit, page:pag, data: events }
   end
 
   def create
-    #checar se existe nesse horario
-
     params[:user_id] = current_user.id
     event = Event.new(event_params)
     event.save!
@@ -28,17 +26,9 @@ class Api::Events::EventsController < ApplicationApiController
   end
 
   private
+
   def event_params
     params.permit(:name, :event_hour_start, :event_hour_end, :event_week, :user_id)
-  end
-
-  def set_user_create
-    @user = User.find_by_id(current_user.id)
-    unless @user
-      render :status => 200, :json => {success: false, erros: "User deve existir"}
-    else
-      @user
-    end
   end
 
   def set_event_edit
@@ -59,31 +49,29 @@ class Api::Events::EventsController < ApplicationApiController
   end
 
   def check_conflicts_time
-    #Event.where('start_date < ? AND end_date > ? AND user_id = ?', date, date, current_user.id)
     #Lista todos os eventos do usuario atual
     @my_events = Event.where(user_id: current_user.id)
     @my_events.each do |event|
-      #verifica o dia da semana de cada evento
+      #verifica o dia da semana de cada evento (do banco)
       event.event_week.split(',').each.with_index do |week, i|
         #se o dia da semana estiver ativo, verifica as hora pra esse dia
-        if week == "1"
-
-          params[:event_week].split(",").each.with_index do |p_week,j|
-            if p_week == "1"
+        if week.to_i == 1
+          #compara a semana enviada(params) com a semanas do banco de dados
+          params[:event_week].split(",").each.with_index do |param_week,j|
+            if param_week.to_i  == 1
               jj = j+1
-              c = @my_events.where("split_part(event_week, ',', #{jj}) = ?", "1").where("event_hour_start <= ? AND event_hour_end >= ?",params[:event_hour_start],params[:event_hour_end]).count
-              print c
-              if c >= 1
-                render :status => 200, :json => {success: false, erros: "Já existe um horario para este dia da semana"}
+              #a magica vem agora (verificar se existe o evento no intervalo de tempo ja cadastrado)
+              count = @my_events.where("split_part(event_week, ',', #{jj}) = ?", "1").where("event_hour_start > ? OR event_hour_end > ?",params[:event_hour_start],params[:event_hour_start]).where("event_hour_start < ? OR event_hour_end < ?",params[:event_hour_end],params[:event_hour_end]).count
+              if count >= 1
+                render :status => 200, :json => {success: false, erros: "Já existe um horário para este dia da semana. Dia: #{t('date.abbr_day_names')[jj]}"}
                 return true
               end
             end
           end
-
-
         end
       end
     end
+    #zerar variavel
     @my_events = nil
   end
 end
