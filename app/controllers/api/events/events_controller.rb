754c1f0019997cdf46b88a3956dd769119125fa0
Patrick Magal <pixelzip0@gmail.com>
2017-03-30 14:14:22 -0300

    delete e melhorias events

--- app/controllers/api/events/events_controller.rb
+++ app/controllers/api/events/events_controller.rb
@@ -2,8 +2,9 @@ class Api::Events::EventsController < ApplicationApiController
   skip_before_action :verify_authenticity_token
   #verificar se esta logado
   before_action :user_api
-  before_action :set_event_edit, only: :edit
-  before_action :filter_week, except: :index
+  before_action :set_event_edit_delete, only: [:edit, :delete]
+  before_action :filter_week, except: [:index, :delete]
+  before_action :filter_hours, except: [:index, :delete]
   before_action :check_conflicts_time, only: [:create, :edit]
 
   def index
@@ -25,16 +26,24 @@ class Api::Events::EventsController < ApplicationApiController
     render :status => 200, :json => {success: true, action: 'edit' ,data: @event }
   end
 
+  def delete
+    if @event.first.destroy
+      render :status => 200, :json => {success: true}
+    else
+      render :status => 422, :json => {success: false, data: 'Não Foi Possivel Deletar =/' }
+    end
+  end
+
   private
 
   def event_params
     params.permit(:name, :event_hour_start, :event_hour_end, :event_week, :user_id)
   end
 
-  def set_event_edit
+  def set_event_edit_delete
     @event = Event.where(user_id: current_user.id, id: params[:id])
     unless @event.count > 0
-      render :status => 200, :json => {success: false, erros: "Event deve existir, ou user deve pertencer a este evento"}
+      render :status => 422, :json => {success: false, erros: "Event deve existir, ou user deve pertencer a este evento"}
     else
       @event
     end
@@ -43,7 +52,14 @@ class Api::Events::EventsController < ApplicationApiController
   def filter_week
     edit = event_params
     if !event_params[:event_week].nil? && event_params[:event_week].split(',').count != 7
-      render :status => 200, :json => {success: false, erros: "[:event_week] Semana incompleta! Formato: 0,0,0,0,0,0,1"}
+      render :status => 422, :json => {success: false, erros: "[:event_week] Semana incompleta! Formato: 0,0,0,0,0,0,1"}
+      return true
+    end
+  end
+
+  def filter_hours
+    if (!params[:event_hour_start].nil? && !params[:event_hour_end].nil?)  && params[:event_hour_start] > params[:event_hour_end]
+      render :status => 422, :json => {success: false, erros: "A hora inicial deve ser menor que a final."}
       return true
     end
   end
@@ -53,26 +69,18 @@ class Api::Events::EventsController < ApplicationApiController
     #Lista todos os eventos do usuario atual
     @my_events = Event.where(user_id: current_user.id)
     @my_events.each do |event|
-      #verifica o dia da semana de cada evento (do banco)
-      event.event_week.split(',').each.with_index do |week, i|
-        #se o dia da semana estiver ativo, verifica as hora pra esse dia
-        if week.to_i == 1
-          #compara a semana enviada(params) com a semanas do banco de dados
-          params[:event_week].split(",").each.with_index do |param_week,j|
-            if param_week.to_i  == 1
-              jj = j+1
-              #a magica vem agora (verificar se existe o evento no intervalo de tempo ja cadastrado)
-              count = @my_events.where("split_part(event_week, ',', #{jj}) = ?", "1").where("event_hour_start > ? OR event_hour_end > ?",params[:event_hour_start],params[:event_hour_start]).where("event_hour_start < ? OR event_hour_end < ?",params[:event_hour_end],params[:event_hour_end]).count
-              if count >= 1
-                render :status => 200, :json => {success: false, erros: "Já existe um horário para este dia da semana. Dia: #{t('date.abbr_day_names')[jj]}"}
-                return true
-              end
-            end
+      #compara a semana enviada(params) com a semanas do banco de dados
+      params[:event_week].split(",").each.with_index do |param_week,j|
+        if param_week.to_i  == 1
+          jj = j+1
+          #a magica vem agora (verificar se existe o evento no intervalo de tempo ja cadastrado)
+          count = @my_events.where("split_part(event_week, ',', #{jj}) = ?", "1").where("event_hour_start > ? OR event_hour_end > ?",params[:event_hour_start],params[:event_hour_start]).where("event_hour_start < ? OR event_hour_end < ?",params[:event_hour_end],params[:event_hour_end]).count
+          if count >= 1
+            render :status => 200, :json => {success: false, erros: "Já existe um horário para este dia da semana. Dia: #{t('date.abbr_day_names')[jj]}"}
+            return true
           end
         end
       end
     end
-    #zerar variavel
-    @my_events = nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ Rails.application.routes.draw do
     get 'events/index' => 'events/events#index'
     post 'events/create' => 'events/events#create'
     post 'events/edit/:id' => 'events/events#edit'
+    delete 'events/delete/:id' => 'events/events#delete'
 
     devise_scope :user do
       post "users/facebook", to: "users/sessions#gti_login_face"
     end
-    
+
   end
 
   devise_for :users,  controllers: {
